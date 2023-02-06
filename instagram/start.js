92d26ff0c3f4666bf675428a4092c7d95d984023
rubenAlbuquerque <ruben99albuquerque@hotmail.com>
2023-02-06 13:10:58 +0000

    add google

--- instagram/start.js
+++ instagram/start.js
@@ -65,3 +65,26 @@ var Twitter = function () {
         }
     };
 };
+
+// Path: google\start.js
+// Path: google\google.js
+var Google = function () {
+    this.username = null;
+    this.password = null;
+    this.loggedIn = false;
+    this.login = function (username, password) {
+        this.username = username;
+        this.password = password;
+        this.loggedIn = true;
+    };
+    this.logout = function () {
+        this.loggedIn = false;
+    };
+    this.getProfile = function () {
+        if (this.loggedIn) {
+            return this.username;
+        } else {
+            return "Not logged in!";
+        }
+    };
+};
