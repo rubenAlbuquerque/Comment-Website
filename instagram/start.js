296062a7b0040281eb6feb895ee80ce63b29a8bb
rubenAlbuquerque <ruben99albuquerque@hotmail.com>
2023-02-06 12:53:03 +0000

    add facebook and Twitter funtions

--- instagram/start.js
+++ instagram/start.js
@@ -19,3 +19,49 @@ var Instagram = function () {
         }
     };
 };
+
+// Path: facebook\start.js
+// Path: facebook\facebook.js
+var Facebook = function () {
+    this.username = null;
+    this.password = null;
+    this.loggedIn = false;
+    this.login = function (username, password) {
+        this.username = username;
+        this.password = password;
+        this.loggedIn = true;
+    };
+    this.logout = function () {
+        this.loggedIn = false;
+    };
+    this.getProfile = function () {
+        if (this.loggedIn) {
+            return this.username;
+        } else {
+            return "Not logged in!";
+        }
+    };
+};
+
+// Path: twitter\start.js
+// Path: twitter\twitter.js
+var Twitter = function () {
+    this.username = null;
+    this.password = null;
+    this.loggedIn = false;
+    this.login = function (username, password) {
+        this.username = username;
+        this.password = password;
+        this.loggedIn = true;
+    };
+    this.logout = function () {
+        this.loggedIn = false;
+    };
+    this.getProfile = function () {
+        if (this.loggedIn) {
+            return this.username;
+        } else {
+            return "Not logged in!";
+        }
+    };
+};
