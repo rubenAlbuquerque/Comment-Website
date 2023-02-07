5c658fe0cbdb4710840a73c3c88ef65acc886d9d
rubenAlbuquerque <ruben99albuquerque@hotmail.com>
2023-02-07 01:53:42 +0000

    home paga updates

--- README.md
+++ README.md
@@ -1,6 +1,22 @@
 # Comment-Website
 
-
-
-
 #### escrever readme
+
+#### Comandos:
+
+npx create-next-app -e with-tailwindcss project
+npm run dev
+
+#### install
+
+npm install @heroicons/react
+npm install @tailwindcss/forms
+
+react,
+heroicons,
+tailwindcss,
+nextjs
+
+npm install --save @fortawesome/fontawesome-svg-core
+npm install --save @fortawesome/free-solid-svg-icons
+npm install --save @fortawesome/react-fontawesome
---
+++ instagram/insta/pages/index.tsx
@@ -0,0 +1,22 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Header from "../components/Header";
+
+const Home: NextPage = () => {
+    return (
+        <div className="">
+            <Head>
+                <title>Comment App</title>
+                <link rel="icon" href="/logo.png" />
+            </Head>
+
+            <Header />
+
+            {/* feed */}
+
+            {/* Model */}
+        </div>
+    );
+};
+
+export default Home;
